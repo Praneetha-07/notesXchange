411f41462c408fb4b752d4f22de2446a265ff3cd
Praneetha-07 <ravuripraneetha7@gmail.com>
2026-04-19 11:25:59 +0530

    Changed uploads path in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,12 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // app.use(
 //   "/uploads",
 //   express.static(path.join(process.cwd(), "backend/uploads")),
 // );
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
